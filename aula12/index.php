916a8fc58d3cdbefaabc73f9bbcd919797d0c0de
Johne Lima <johne.keyvid0@gmail.com>
2023-08-26 16:04:19 -0300

    aula 12 de php

---
+++ aula12/index.php
@@ -0,0 +1,7 @@
+<?php
+//Arquivo index responsavel pela inicialização do sistema
+
+require_once 'configuracao.php'; //include (incluir), require ((requirir) arquivo tem que existir).
+include_once 'helpers.php';
+
+echo '<h1>Arquivo index</h1>';
